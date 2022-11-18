ae0d2303e0ad61d5bb35097f70b513e10fb877fd
Machew115 <why.mherrera@gmail.com>
2022-11-18 13:40:31 -0700

    part 4 practice

--- functions_practice.py
+++ functions_practice.py
@@ -1,21 +1,60 @@
-def hello():
-    print("hello user")
-hello()
-def pack(a, b, c):
-    return [a, b, c]
+#Write a Python function called max_num()to find the maximum of three numbers.
+def max_num(a, b, c):
+    list = [a, b, c]
+    return max(list)
 
-print(pack(1, 2, 3)) 
+print(max_num(1, 5, 10))
+#Write a Python function called mult_list() to multiply all the numbers in a list.
+def multiply(*num):
+    total = 1
+    for x in num:
+        total *= x
+    return total
+print(multiply(1, 3, 9, 12))
+#Write a Python function called rev_string() to reverse a string.
+def rev_string(string):
+    text = string[::-1]
+    print(text)
 
-def eatlunch(food):
-    if len(food) == 0:
-        print("My Lunch Box is Empty")
+rev_string("hello")
+#Write a Python function called num_within() to check whether a number falls in a given range.
+def num_within(num):
+    if num > 10 and num <20:
+        print(num)
     else:
-        for i in range(len(food)):
-            if i == 0:
-                print(f"first I eat {food[0]}")
-            else:
-                print(f"next I eat{food[i]}")
+        print("say what?")
 
-eatlunch([])
-eatlunch(["sandwich, apple, banana"])
-eatlunch(["apple","banana","sandwich","cookie"])
+num_within(12)
+#The function accepts the number, beginning of range, and end of range (inclusive) as arguments.
+def num_within(x,a,b):
+  return x in range(a,b+1)
+
+#Examples: num_within(3,2,4) returns True, num_within(3,1,3) returns True, num_within(10,2,5) returns False.
+
+#Write a Python function called pascal() that prints out the first n rows of Pascal's triangle.
+
+#The function accepts the number n, the number of rows to print
+triangle = [[1],[1,1]]
+def pascal(n):
+  if n < 1:
+    print("invalid number of rows")
+  elif n == 1:
+    print(triangle[0])
+  else:
+    row_number = 2
+    while len(triangle) < n:
+      row = []
+      row_prev = triangle[row_number - 1]
+      length = len(row_prev)+1
+      for i in range(length):
+        if i == 0:
+          row.append(1)
+        elif i > 0 and i < length-1:
+          row.append(triangle[row_number-1][i-1]+triangle[row_number-1][i])
+        else:
+          row.append(1)
+      triangle.append(row)
+      row_number += 1
+    for row in triangle:
+      print(row)
+#Note : Pascal's triangle is an arithmetic and geometric figure first imagined by Blaise Pascal. Each number is the two numbers above it added together.
--- python_setup.py
+++ python_setup.py
@@ -1,1 +1,4 @@
 print("Hello from inside a file!")
+
+#to run you practice function remember git checkout 
+#to test run python3 functions_practice.py
